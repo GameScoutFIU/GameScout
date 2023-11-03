e02d1a7059d8beeecb87c0accf5a0118c4770d3d
Erick <erk.rivs@gmail.com>
2023-11-03 14:11:55 -0400

    Adding Swift Project

---
+++ GameScout/GameScout/GameScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameScoutApp.swift
+//  GameScout
+//
+//  Created by Erick Rivera on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GameScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
